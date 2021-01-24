360be2e1eb1d90ebfc5e9b8cd78771ae1ce91a26
Apurva <apurvanagar@gmail.com>
2021-01-24 11:38:32 +0530

    created users table

---
+++ db/migrate/20210124060430_add_email_password_to_user.rb
@@ -0,0 +1,6 @@
+class AddEmailPasswordToUser < ActiveRecord::Migration[6.1]
+  def change
+    add_column :users, :email, :string
+    add_column :users, :password_digest, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_24_050419) do
+ActiveRecord::Schema.define(version: 2021_01_24_060430) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,15 @@ ActiveRecord::Schema.define(version: 2021_01_24_050419) do
     t.decimal "price"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "role"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "email"
+    t.string "password_digest"
   end
 
 end

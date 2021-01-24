75591a880f5517af6cc16ca7e4d8d7c52ebcb0a3
Apurva <apurvanagar@gmail.com>
2021-01-24 11:07:31 +0530

    controller to add menu items

--- app/controllers/menu_items_controller.rb
+++ app/controllers/menu_items_controller.rb
@@ -1,5 +1,21 @@
 class MenuItemsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def index
     render plain: MenuItem.all.map { |item| item.to_pleasant_string }.join("\n")
+  end
+
+  def create
+    name = params[:name]
+    price = params[:price]
+    new_menu_item = MenuItem.new(
+      name: name,
+      price: price,
+    )
+    if new_menu_item.save
+      render plain: "Menu Item Added"
+    else
+      render plain: "ERROR: Menu item not added"
+    end
   end
 end

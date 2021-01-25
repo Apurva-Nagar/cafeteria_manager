5a07ff5bc5d83ad088f80e2ea3e2f0fa3c464747
Apurva <apurvanagar@gmail.com>
2021-01-25 11:02:33 +0530

    removed a show function from menu and menu_item models

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,7 +1,3 @@
 class Menu < ActiveRecord::Base
   has_many :menu_items
-
-  def to_pleasant_string
-    "#{id}. | #{name}"
-  end
 end
--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -1,7 +1,3 @@
 class MenuItem < ActiveRecord::Base
   belongs_to :menu, optional: true
-
-  def to_pleasant_string
-    "#{id}. #{name} #{price}"
-  end
 end

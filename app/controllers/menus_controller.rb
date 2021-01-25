c1993fb2da9b6c1467d82e53f56e30737c7985d0
Apurva <apurvanagar@gmail.com>
2021-01-25 16:10:09 +0530

    can now menus as active. only one menus can be active at a time

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -9,11 +9,25 @@ class MenusController < ApplicationController
     name = params[:name]
     new_menu = Menu.new(
       name: name,
+      active: false,
     )
     if new_menu.save
       redirect_to menu_items_path
     else
       render plain: "ERROR: Menu not added"
     end
+  end
+
+  def update
+    id = params[:id]
+    current_active = Menu.find_by active: "true"
+    current_active.active = false
+    current_active.save
+
+    update_active_menu = Menu.find(id)
+    update_active_menu.active = true
+    update_active_menu.save
+
+    redirect_to menus_path
   end
 end

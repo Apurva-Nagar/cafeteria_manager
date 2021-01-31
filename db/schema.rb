f27db52803d070a8980afec94975bc18b8bb6376
Apurva <apurvanagar@gmail.com>
2021-01-31 20:08:10 +0530

    can place order from cart now

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_30_175821) do
+ActiveRecord::Schema.define(version: 2021_01_31_143108) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -83,6 +83,7 @@ ActiveRecord::Schema.define(version: 2021_01_30_175821) do
     t.decimal "menu_item_price"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "quantity"
   end
 
   create_table "orders", force: :cascade do |t|

904f25db7dfaadf7edcbf683fbd05ace448545ed
Apurva <apurvanagar@gmail.com>
2021-02-06 16:15:23 +0530

    moved cart_item creation logic from controller to model

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -4,30 +4,15 @@ class CartItemsController < ApplicationController
     menu_item_info = params[:menu_item_info]
     cart_total = Cart.get_current_user_cart(@current_user.id).total
 
-    menu_item_info.each do |item|
-      id = item["info"]["menu_item_id"] ? item["info"]["menu_item_id"] : nil
-      if id
-        cart_item = CartItem.new(
-          cart_id: user_cart_id,
-          menu_item_id: id,
-          menu_item_name: MenuItem.find(id).name,
-          menu_item_price: MenuItem.find(id).price,
-          menu_item_quantity: item["info"]["quantity"],
-        )
-        cart_total += (cart_item.menu_item_price * cart_item.menu_item_quantity)
-        if !cart_item.save
-          flash[:error] = "Couldn't add item to cart."
-          redirect_to menus_path
-          return
-        end
-      end
+    cart_status = CartItem.add_to_cart(user_cart_id, menu_item_info, cart_total)
+
+    if cart_status
+      redirect_to cart_index_path
+    else
+      flash[:error] = "Couldn't add items to cart."
+      redirect_to menus_path
+      return
     end
-
-    Cart.find(user_cart_id).update(
-      total: cart_total,
-    )
-
-    redirect_to cart_index_path
   end
 
   def increase
--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,4 +1,27 @@
 class CartItem < ActiveRecord::Base
   belongs_to :cart
   belongs_to :menu_item
+
+  def self.add_to_cart(user_cart_id, menu_item_info, cart_total)
+    menu_item_info.each do |item|
+      id = item["info"]["menu_item_id"] ? item["info"]["menu_item_id"] : nil
+      if id
+        cart_item = CartItem.new(
+          cart_id: user_cart_id,
+          menu_item_id: id,
+          menu_item_name: MenuItem.find(id).name,
+          menu_item_price: MenuItem.find(id).price,
+          menu_item_quantity: item["info"]["quantity"],
+        )
+        cart_total += (cart_item.menu_item_price * cart_item.menu_item_quantity)
+        if !cart_item.save
+          return false
+        end
+      end
+    end
+
+    Cart.find(user_cart_id).update(
+      total: cart_total,
+    )
+  end
 end

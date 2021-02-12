39d84b178ba85ef3a8131937dee48bc9e8e79f5d
Apurva <apurvanagar@gmail.com>
2021-02-12 21:28:01 +0530

    using database transaction while creating orders

--- app/models/order.rb
+++ app/models/order.rb
@@ -15,33 +15,27 @@ class Order < ActiveRecord::Base
   end
 
   def self.create_order(date, user_id, cart)
-    new_order = Order.new(
-      date: date,
-      user_id: user_id,
-      delivered: false,
-      total: cart.total,
-    )
-
-    if new_order.save
+    ActiveRecord::Base.transaction do
+      new_order = Order.create!(
+        date: date,
+        user_id: user_id,
+        delivered: false,
+        total: cart.total,
+      )
       cart.cart_items.each do |item|
-        order_item = OrderItem.new(
+        OrderItem.create!(
           order_id: new_order.id,
           menu_item_id: item.menu_item_id,
           menu_item_name: item.menu_item_name,
           menu_item_price: item.menu_item_price,
           quantity: item.menu_item_quantity,
         )
-        if !order_item.save
-          return false
-        end
       end
       cart.cart_items.delete_all
-      cart.update(
-        total: 0.0,
-      )
-    else
-      return false
+      cart.update!(total: 0.0)
     end
+  rescue ActiveRecord::RecordInvalid => exception
+    false
   end
 
   def self.get_report_orders(start_date, end_date)

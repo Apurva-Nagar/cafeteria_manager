0bc97b89877f07bb82356c2cda24310493168b89
Aditya  Sunit Kanoi <77881724+Adityakanoi2001@users.noreply.github.com>
2023-08-23 17:31:17 +0530

    Update application_controller.rb
    We use the before_action filter to ensure that ensure_user_logged_in is called before any action in the controller. This ensures that only authenticated users can access these actions.
    
    We use User.find_by(id: ...) instead of User.find to handle cases where the user with the provided current_user_id doesn't exist.
    
    We use root_path instead of "/" for redirects. Make sure you have a proper route defined for the root path in your config/routes.rb file.
    
    We use current_user.is_owner? and current_user.is_clerk? to check the user's roles, assuming these are boolean methods in your User model. This is more maintainable than directly checking for role flags.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,31 +1,27 @@
 class ApplicationController < ActionController::Base
   before_action :ensure_user_logged_in
 
+  protected
+
   def ensure_user_is_owner_or_clerk
-    unless current_user && (current_user.is_owner || current_user.is_clerk)
-      redirect_to "/"
+    unless current_user && (current_user.is_owner? || current_user.is_clerk?)
+      redirect_to root_path
     end
   end
 
   def ensure_user_is_owner
-    unless current_user && current_user.is_owner
-      redirect_to "/"
+    unless current_user && current_user.is_owner?
+      redirect_to root_path
     end
   end
 
   def ensure_user_logged_in
     unless current_user
-      redirect_to "/"
+      redirect_to root_path
     end
   end
 
   def current_user
-    return @current_user if @current_user
-    current_user_id = session[:current_user_id]
-    if current_user_id
-      @current_user = User.find(current_user_id)
-    else
-      nil
-    end
+    @current_user ||= User.find_by(id: session[:current_user_id])
   end
 end
